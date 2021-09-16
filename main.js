ef8a89c27dba4c9963cba912365cc55671f46e1b
Rhythm Munshi <programmingwithrm@gmail.com>
2021-09-16 13:41:06 +0600

    Completed Website

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
-function getPin() {
-    const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + '';
-    if (pinString.length == 4) {
-        return pin;
+document.getElementById('generate').addEventListener('click' , function(){
+    const randomValue = Math.random()*10000;
+    result = Math.floor(randomValue);
+    console.log(result)
+    let string = result.toString();
+    let length = string.length;
+    if(length==4){
+        document.getElementById('show').innerText = result;
     }
-    else {
-        // console.log('got 3 digit and calling again', pin);
-        return getPin();
+    else{
+        document.getElementById('show').innerText = 'Try Again';
     }
-}
-function generatePin() {
-    const pin = getPin();
-    document.getElementById('display-pin').innerText = pin;
-}
+})
 
-document.getElementById('key-pad').addEventListener('click', function (event) {
-    const number = event.target.innerText;
-    const calcInput = document.getElementById('typed-numbers');
-    if (isNaN(number)) {
-        if (number == 'C') {
-            calcInput.value = '';
+var input = document.getElementById('input');
+number = document.querySelectorAll('.num div');
+clear = document.getElementById('clear');
+resultDisplayed = false;
+
+for(var i = 0; i< number.length; i++){
+    number[i].addEventListener("click", function(e){
+        var currentString = input.innerHTML;
+        if(resultDisplayed == false){
+           out = input.innerHTML += e.target.innerHTML;
         }
-    }
-    else {
-        const previousNumber = calcInput.value;
-        const newNumber = previousNumber + number;
-        calcInput.value = newNumber;
-    }
-});
-
-function verifyPin() {
-    const pin = document.getElementById('display-pin').innerText;
-    const typedNumbers = document.getElementById('typed-numbers').value;
-    const successMessage = document.getElementById('notify-success');
-    const failError = document.getElementById('notify-fail');
-    if (pin == typedNumbers) {
-        successMessage.style.display = 'block';
-        failError.style.display = 'none';
-    }
-    else {
-        successMessage.style.display = 'none';
-        failError.style.display = 'block';
-    }
+    });
 }
+
+clear.addEventListener('click', function(){
+    input.innerHTML = '';
+    document.getElementById('match').style.display = 'none';
+    document.getElementById('notMatch').style.display = 'none';
+
+})
+
+
+document.getElementById('submit').addEventListener('click', function(){
+    console.log(typeof output);
+    if(result==out){
+        document.getElementById('match').style.display = 'block';
+    }
+    else{
+        document.getElementById('notMatch').style.display = 'block';
+        // output = document.getElementById('count').innerText;
+        // output = parseFloat(output);
+        // let retry= output-1;
+        // document.getElementById('count').innerText = retry;
+        // console.log(retry)
+}
+})
+
